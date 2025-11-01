297edf822a0db0db61d686e9ac018763dad544ca
avhixorin <avhixorin@gmail.com>
2025-11-02 00:04:03 +0530

    feat: enhance RootLayout with AI chat modal and floating button

--- client/src/pages/rootlayout.tsx
+++ client/src/pages/rootlayout.tsx
@@ -1,16 +1,25 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 import { Sidebar } from "@/components/sidebar/sidebar";
 
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
 import { useScreenSizeStore } from "@/store/screenSizestate.store";
+import { useState } from "react";
+import AIChatModal, { FloatingAIButton } from "@/components/aiModal";
 
 export const RootLayout: React.FC = () => {
-    const mobileView = useScreenSizeStore((state) => state.mobileView);
-    return (
-        <main className={`flex ${mobileView && "flex-col-reverse"} h-dvh w-screen bg-background text-foreground `}>
-            <Sidebar />
-            <Outlet />
-            <Toaster closeButton richColors position="top-center" />
-        </main>
-    )
-}
+  const mobileView = useScreenSizeStore((state) => state.mobileView);
+  const [isAiModalOpen, setIsAiModalOpen] = useState(false);
+  return (
+    <main
+      className={`flex ${
+        mobileView && "flex-col-reverse"
+      } h-dvh w-screen bg-background text-foreground `}
+    >
+      <Sidebar />
+      <Outlet />
+      {isAiModalOpen && <AIChatModal setIsAiModalOpen={setIsAiModalOpen} />}
+      {!isAiModalOpen && <FloatingAIButton setIsAiModalOpen={setIsAiModalOpen} />}
+      <Toaster closeButton richColors position="top-center" />
+    </main>
+  );
+};
